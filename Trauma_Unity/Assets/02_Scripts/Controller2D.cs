2e72e3732288eed2170f12329fdcd43c434e4b8d
DimitriCalleau <dimitri.calleau@outlook.fr>
2020-02-05 16:04:52 +0100

    code

---
+++ Trauma_Unity/Assets/02_Scripts/Controller2D.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    public GameObject Camera;
+    public bool pause;
+
+    //Movement
+    public float forceMvt;
+    public float maxSpeedGround;
+    //Jump
+    public float jumpForce;
+    public int maxJump;
+    public int nbJump;
+    public bool isJumping;
+
+    public Rigidbody2D Rigidbody { get => rigidbody; set => rigidbody = value; }
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pause = Camera.GetComponent<SceneAndUI>().pause;
+        if (pause == false)
+        {
+            bool GoToMenu = Input.GetButtonDown("Cancel");
+            bool attack = Input.GetButtonDown("Fire1");
+
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            bool jump = Input.GetButtonDown("Jump");
+
+            if (h > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            if (h < 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+
+            if (h != 0)
+            {
+                rigidbody.AddForce((Vector2.right * forceMvt) * h);
+            }
+            if (h == 0)
+            {
+            }
+
+
+            if (rigidbody.velocity.x >= maxSpeedGround)
+            {
+                rigidbody.velocity = new Vector2(maxSpeedGround, rigidbody.velocity.y);
+            }
+            if (rigidbody.velocity.x <= -maxSpeedGround)
+            {
+                rigidbody.velocity = new Vector2(-maxSpeedGround, rigidbody.velocity.y);
+            }
+
+            // le saut
+
+            if (nbJump < maxJump && jump == true)
+            {
+                Rigidbody.AddForce(Vector2.up * jumpForce);
+                nbJump += 1;
+                isJumping = true;
+            }
+        }
+
+    }
+
+    //Détecte les collisions(pour le jump et mort)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Saut
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            nbJump = 0;
+            isJumping = false;
+        }
+
+    }
+}
---
+++ Trauma_Unity/Assets/02_Scripts/SceneAndUI.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+public class SceneAndUI : MonoBehaviour
+{
+    //SceneManagement
+    string activeScene;
+
+    //Ui
+    public bool pause;
+    private bool isPaused;
+    public GameObject pausePanel;
+
+    private void Start()
+    {
+        pause = false;
+        isPaused = false;
+        pausePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            pause = !pause;
+        }
+
+        if(pause == true)
+        {
+            if(isPaused == false)
+            {
+                pausePanel.SetActive(true);
+                isPaused = true;
+            }
+        }
+        else
+        {
+            if(isPaused == true)
+            {
+                pausePanel.SetActive(false);
+                isPaused = false;
+            }
+        }
+    }
+
+    public void SceneLoader(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+        activeScene = sceneName;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(activeScene);
+    }
+
+    public void Resume()
+    {
+        pause = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void SaveProgression()
+    {
+        Debug.Log("Save");
+    }
+
+    public void LoadLastSave()
+    {
+        Debug.Log("Load");
+    }
+}
