e71cd63083b3b5ac2192036c28c082bf4eb12b8a
DimitriCalleau <dimitri.calleau@outlook.fr>
2020-04-27 21:10:17 +0200

    saut joie checkpoint

--- Assets/02_Scripts/CheckPoint.cs
+++ Assets/02_Scripts/CheckPoint.cs
@@ -6,7 +6,8 @@ public class CheckPoint : MonoBehaviour
 {
     public GameObject gameManager;
     int nbLvl;
-    public bool state;
+    public int state;
+    public bool done;
     //Jump
     public float jump;
     public float speed;
@@ -21,33 +22,39 @@ public class CheckPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        state = false;
         nbLvl = gameManager.GetComponent<GameManager>().nbLvlDone;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.gameObject.tag.Equals("Player"))
         {
             if(nbLvl == 0)
             {
-                if(state == false)
+                if (state == 0)
                 {
-                    collision.gameObject.GetComponent<Controller2D>().jumpForce = modifiedJump;
-                    collision.gameObject.GetComponent<Controller2D>().mvtSpeed = modifiedSpeed;
-                    collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = modifiedGravity;
-                    collision.gameObject.GetComponent<Controller2D>().camera2D.GetComponent<Camera>().orthographicSize = cameraDistanceAfter;
-                    state = true;
+                    if (done == false)
+                    {
+                        state = 1;
+                        collision.gameObject.GetComponent<Controller2D>().jumpForce = modifiedJump;
+                        collision.gameObject.GetComponent<Controller2D>().mvtSpeed = modifiedSpeed;
+                        collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = modifiedGravity;
+                        collision.gameObject.GetComponent<Controller2D>().camera2D.GetComponent<Camera>().orthographicSize = cameraDistanceAfter;
+                        done = true;
+                    }
                 }
-                if(state == true)
+                if (state == 1)
                 {
-                    collision.gameObject.GetComponent<Controller2D>().jumpForce = jump;
-                    collision.gameObject.GetComponent<Controller2D>().mvtSpeed = speed;
-                    collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = gravity;
-                    collision.gameObject.GetComponent<Controller2D>().camera2D.GetComponent<Camera>().orthographicSize = cameraDistanceBefore;
-                    state = false;
-                }
+                    if (done == false)
+                    { 
+                        collision.gameObject.GetComponent<Controller2D>().jumpForce = jump;
+                        collision.gameObject.GetComponent<Controller2D>().mvtSpeed = speed;
+                        collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = gravity;
+                        collision.gameObject.GetComponent<Controller2D>().camera2D.GetComponent<Camera>().orthographicSize = cameraDistanceBefore;
+                        state = 0;
+                        done = true;
+                    }
 
+                }
             }
 
             if(nbLvl == 4)
@@ -55,6 +62,14 @@ public class CheckPoint : MonoBehaviour
                 collision.gameObject.GetComponent<Controller2D>().stackTristesse += 1;
                 Destroy(this.gameObject);
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            done = false;
         }
     }
 }
