e6ce51893f4d8b5e632878c90040ff75e7d0d631
DimitriCalleau <dimitri.calleau@outlook.fr>
2020-04-29 01:09:38 +0200

    platerforme tournante

---
+++ Assets/02_Scripts/Tourbillon2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tourbillon2D : MonoBehaviour
+{
+    public float timer;
+    public float timing;
+
+    void Update()
+    {
+        if(timer <= 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z + 90);
+
+            timer = timing;
+        }
+
+        if(timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
