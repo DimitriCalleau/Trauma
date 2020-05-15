fff8ccfdc6dd1fef8033b7bbf9e347f35e051b4c
DimitriCalleau <dimitri.calleau@outlook.fr>
2020-05-15 17:16:57 +0200

    peur

---
+++ Assets/02_Scripts/ChekcpointPeur.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChekcpointPeur : MonoBehaviour
+{
+    public GameObject camera2D;
+    public float cameraSize;
+    public Vector3 newCameraPosition;
+
+    public GameObject enemyPeur;
+    public Vector3 enemyPosition;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            camera2D.GetComponent<Camera>().orthographicSize = cameraSize;
+            camera2D.GetComponent<CameraFollow>().offset = newCameraPosition;
+            enemyPeur.transform.position = enemyPosition;
+        }
+    }
+}        
---
+++ Assets/02_Scripts/Eye_Animation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye_Animation : MonoBehaviour
+{
+    public Animator anm;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            anm.SetBool("Detected", true);
+        }
+    }
+}
