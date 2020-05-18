6dea3156e12aa657cd318144d1fd76381fc164d4
DimitriCalleau <dimitri.calleau@outlook.fr>
2020-05-18 15:51:02 +0200

    plateforme qui bouge de droite a gauche

---
+++ Assets/02_Scripts/PlateformeSlide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateformeSlide : MonoBehaviour
+{
+    public float range;
+    public float speed;
+    private Vector2 startPos;
+    private Vector2 V;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        V = startPos;
+        V.x += range * Mathf.Sin(Time.deltaTime * speed);
+        transform.position = V;
+    }
+}
