4374168d10854df00d810258dcb7ce7cb818d8d8
DimitriCalleau <dimitri.calleau@outlook.fr>
2020-05-25 11:06:38 +0200

    Essaie_Tiroir

---
+++ Assets/02_Scripts/PickableObjectBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObjectBehaviour : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Tiroir"))
+        {
+            transform.SetParent(collision.gameObject.transform);
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
