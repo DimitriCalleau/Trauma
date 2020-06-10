85a355a677a514b8c84e48560647fcf8268d0d3e
DimitriCalleau <dimitri.calleau@outlook.fr>
2020-06-10 22:16:08 +0200

    fjfd

--- Assets/02_Scripts/Colere/BlockDestructible.cs
+++ Assets/02_Scripts/Colere/BlockDestructible.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BlockDestructible : MonoBehaviour
 {
+    public Sprite normal;
+    public Sprite casse;
+
     public void BreakingAnim()
     {
         Destroy(this.gameObject, 0.2f);
